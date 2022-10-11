6ae67c40c8dd9d0dbe714ff68240a383cffa36b0
Omkar Raut <omkarraut2412@gmail.com>
2022-10-11 14:44:39 +0530

    made changes in code

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [{
+        id: "1",
+        Name: "omkar",
+        Age: "23"
+    },
+    {
+        id: "2",
+        Name: "omkar_r",
+        Age: "24"
+    },
+    {
+        id: "3",
+        Name: "qwerty",
+        Age: "25"
+    }
+]
+
+export default Employees;
